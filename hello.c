5a11f9262886e3e342ca0e8fd6c7df0c491f31b6
mattj2025 <2026matthew@gmail.com>
2025-08-26 12:03:14 -0500

    added major to output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Matthew Johnson
+ * 2025-08-26
+ *
+ * A simple hello world program in c
+ *
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf("Computer Science!!!!\nMatthew JOHNSON!!!!!\n");
+
+    return 0;
+
+ }
