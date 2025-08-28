d9e719af2f5d52d664166bcd7d78c75aff1a7710
mattj2025 <2026matthew@gmail.com>
2025-08-28 11:26:43 -0500

    matt commit

--- hello.c
+++ hello.c
@@ -1,18 +1,19 @@
 /**
- * Matthew Johnson
- * 2025-08-26
+ * Author: Jeyvin Coleman
+ * Date: 2025-8-26
  *
- * A simple hello world program in c
+ * A simple hello world program in C
  *
  */
+#include <stdlib.h>
+#include <stdio.h>
 
- #include <stdlib.h>
- #include <stdio.h>
+int main(int argc, char **argv) {
 
- int main(int argc, char **argv) {
+    printf("My name is Jeyvin!\n");
+    printf("My major is Computer Engineering\n");
+    printf("My name is Matt!!!\n");
+    printf("My major is Computer Science!!");
 
-    printf("Computer Science!!!!\nMatthew JOHNSON!!!!!\n");
-
-    return 0;
-
- }
+  return 0;
+}
